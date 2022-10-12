51ee37327230db43dc3c4ab623c12068b2a224e9
mgradyn <mgradyn@gmail.com>
2022-10-12 23:56:47 +0700

    registration validation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,12 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('gender', 10);
+            $table->date('date_of_birth');
+            $table->string('country');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('role_as')->default('0');
             $table->rememberToken();
             $table->timestamps();
         });
